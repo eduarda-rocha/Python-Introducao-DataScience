03ee9ec1dba72cf645a0b29ff815b9f5bd6cfc01
Barbara <barbararocha@alunos.utfpr.edu.br>
2022-02-01 14:20:03 -0300

    Aula 2 - operações

---
+++ Aula2-OperacoesMatematicas.py
@@ -0,0 +1,31 @@
+## Operações Matemáticas
+
+soma = 2 + 2
+soma
+
+subtracao = 2 - 2 
+subtracao
+
+divisao = 10 / 4
+divisao
+
+divisao_inteira = 10 // 4
+divisao_inteira
+
+multiplicacao = 23 * 2
+multiplicacao
+
+exponenciacao = 2 ** 3
+exponenciacao
+
+resto_divisao = 10 % 4
+resto_divisao
+
+expressao_matematica = 5 * 6 + 3 / 2 - 23 #resolve por ordem de preferencia quando não tem parenteses
+expressao_matematica
+
+_ + 5                                   #variavel "_" underscore atribui o ultimo resultado impresso à ela
+
+
+
+
